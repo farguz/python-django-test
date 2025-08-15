e39537b0481f1bb583e2c4ccab0b48cb13079887
aleksei z <farguzalexey@gmail.com>
2025-08-15 17:38:20 +0300

    add index and about pages

--- python_django_test/urls.py
+++ python_django_test/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import views
+
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('about/', views.about),
 ]

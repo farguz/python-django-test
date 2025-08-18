9010e62ae73e3196435a1a65f57fa23627510896
aleksei z <farguzalexey@gmail.com>
2025-08-18 17:28:26 +0300

    now use View

--- python_django_test/article/views.py
+++ python_django_test/article/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
-
-def index(request):
-    return render(
-        request,
-        "articles/index.html",
-        context={
-            "appname": "ARTICLES",
-        },
-    )
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+                request,
+                "articles/index.html",
+                context={
+                    "appname": "ARTICLES",
+                },
+            )

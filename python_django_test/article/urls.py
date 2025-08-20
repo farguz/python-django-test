6659ea14add02d684e02e54e32b293f6191c3315
aleksei z <farguzalexey@gmail.com>
2025-08-20 17:49:57 +0300

    fix to fit unit 7

--- python_django_test/article/urls.py
+++ python_django_test/article/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("", views.index),
+    # path("", views.index),
     path("", views.IndexView.as_view()),
-]
+    path(
+        '<str:tags>/<int:article_id>/',
+        views.TagPageView.as_view(),
+        name='article'
+        ),
+    ]
--- python_django_test/article/views.py
+++ python_django_test/article/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.urls import reverse
+from django.views.generic.base import TemplateView
+
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
+        return redirect(reverse('article', kwargs={"article_id": 42, "tags": "python"}))
+    
+class TagPageView(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
         return render(
-                request,
-                "articles/index.html",
-                context={
-                    "appname": "ARTICLES",
-                },
-            )
+            request, "articles/index.html", context={"article_id": 42, "tags": "python"},
+        )    

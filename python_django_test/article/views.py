2e9d55b7087244481fc1688476c986abb3aca18c
aleksei z <farguzalexey@gmail.com>
2025-08-26 16:59:29 +0300

    fix to fit crud list practice unit

--- python_django_test/article/views.py
+++ python_django_test/article/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
-from django.urls import reverse
 from django.views.generic.base import TemplateView
-
+from python_django_test.article.models import Article
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return redirect(reverse('article', kwargs={"article_id": 42, "tags": "python"}))
+        articles = Article.objects.all()[:15]
+        return render(
+            request,
+            "articles/index.html",
+            context={
+                "articles": articles,
+            },
+        )
     
 class TagPageView(TemplateView):
     template_name = 'index.html'
